8ece9bf37868e2d490225df992366218531635d8
lcx <1013688053@qq.com>
2016-09-22 18:29:03 +0800

    1

--- Web/Controllers/OrderController.cs
+++ Web/Controllers/OrderController.cs
@@ -114,5 +114,14 @@ namespace Web.Controllers
             return JResult(WebService.Get_PaySelectItem(id));
         }
 
+
+        /// <summary>
+        /// 获取菜品分类选择项
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetThemeSelectItem(string id)
+        {
+            return JResult(WebService.Get_ThemeSelectItem(id));
+        }
     }
 }
